8a669c9508f19618e686f6ed84b78c5349288d20
medright <medright@gmail.com>
2015-04-27 17:03:51 -0400

    added users table

--- db/migrate/20150427210225_create_users.rb
+++ db/migrate/20150427210225_create_users.rb
@@ -1,12 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :kind
       t.string :email
-      t.string :password
-      t.string :password_conf
-      t.integer :pin
-      t.string :role
+      t.string :pin
+      t.string :password_digest
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326013100) do
+ActiveRecord::Schema.define(version: 20150427210225) do
 
   create_table "labs", force: :cascade do |t|
     t.string   "na"
@@ -104,14 +104,12 @@ ActiveRecord::Schema.define(version: 20150326013100) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "kind"
     t.string   "email"
-    t.string   "password"
-    t.string   "password_conf"
-    t.integer  "pin"
-    t.string   "role"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "pin"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "vital_signs", force: :cascade do |t|
