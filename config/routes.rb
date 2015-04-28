a6411619591f0f345b29950780b0f995bd6066f0
medright <medright@gmail.com>
2015-04-28 11:35:00 -0400

    added styles for signin/up

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'meds#index'
+  root 'sessions#new'
   resources :meds
 
   # Example of regular route:
