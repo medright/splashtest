03aaabdbec11924ca5ae9c58451e6fe96d738cad
medright <medright@gmail.com>
2015-05-09 20:55:08 -0400

    updated flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 	def create
 		if user = User.authenticate(params[:email], params[:password], params[:pin])
 			session[:user_id] = user.id
-			flash[:notice] = "Welcome back, #{user.email}!"
+			flash[:notice] = "Welcome back, #{user.email}"
 			redirect_to(session[:intended_url] || user)
 			session[:intended_url] = nil
 		else
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
 	def destroy
 		session[:user_id] = nil
-		redirect_to root_url, notice: "Now you're signed out!"
+		redirect_to root_url, notice: "Now you're signed out"
 	end
 end
